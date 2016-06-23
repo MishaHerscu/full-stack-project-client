0116cbbcfa8a192a4eb363a3fe173ca68ea62e43
MishaHerscu <mishaherscu@gmail.com>
2016-06-23 10:41:46 -0400

    copied in auth code

---
+++ assets/scripts/auth/ui.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const app = require('../app.js');
+
+const success = (data) => {
+  if (data) {
+    console.log(data);
+  } else {
+    console.log('Success');
+  }
+};
+
+const failure = (error) => {
+  console.error(error);
+};
+
+const signInSuccess = (data) => {
+  app.user = data.user;
+  console.log(app.user);
+  $('.signed-in').show();
+  $('.signed-out').hide();
+};
+
+const signOutSuccess = () => {
+  console.log('User signed out successfully');
+  app.user = null;
+  $('.signed-in').hide();
+  $('.signed-out').show();
+};
+
+module.exports = {
+  success,
+  failure,
+  signInSuccess,
+  signOutSuccess
+};
