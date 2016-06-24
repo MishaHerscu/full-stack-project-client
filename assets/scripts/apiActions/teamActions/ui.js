abecced868fceb406e66c1ebd946ac18ff0b2ff3
MishaHerscu <mishaherscu@gmail.com>
2016-06-24 19:44:19 -0400

    show actual games data from DB on standings page with handlebars

--- assets/scripts/apiActions/teamActions/ui.js
+++ assets/scripts/apiActions/teamActions/ui.js
@@ -12,8 +12,41 @@ const failure = (error) => {
   console.error(error);
 };
 
+const updateTeamStats = (team) => {
+
+  team.gameCount = team.games.length;
+
+  team.winCount = 0;
+  team.lossCount = 0;
+  team.tieCount = 0;
+
+  if(team.gameCount > 0){
+    for(let j = 0, max = team.gameCount; j < max; j++){
+      if(team.games[j].won === 'true'){
+        team.winCount += 1;
+      }else if(team.games[j].won === 'false'){
+        team.lossCount += 1;
+      }else{
+        team.tieCount += 1;
+      }
+    }
+    team.winPct = ((team.winCount / team.gameCount) * 100) + '%';
+  }else{
+    team.winPct = 'N/A';
+  }
+
+  return team;
+};
+
 const onShowTeamsSuccess = (data) => {
-  $('.teams-standing').html('');
+  $('#teams-standing').html('');
+
+  for(let i = 0, max = data.teams.length; i < max; i++){
+    console.log(data.teams[i]);
+    data.teams[i] = updateTeamStats(data.teams[i]);
+    console.log(data.teams[i]);
+  }
+
   let teamListingTemplate = require('../../templates/team-listing.handlebars');
   $('.teams-standing').append(teamListingTemplate(data));
 };
