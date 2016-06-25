b5761d962e258c91053bec185ec1e18d5419281c
MishaHerscu <mishaherscu@gmail.com>
2016-06-25 00:29:13 -0400

    added heroku api link

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,8 +1,8 @@
 'use strict';
 
 const app = {
-  host: 'http://localhost:3000',
-  // host: 'https://young-gorge-10009.herokuapp.com',
+  // host: 'http://localhost:3000',
+  host: 'https://ironsidegoaltimate.herokuapp.com',
   user: null,
 };
 
