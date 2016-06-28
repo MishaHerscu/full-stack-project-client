4064b1b974c4664f4cd9cca4b901e91dc8770381
MishaHerscu <mishaherscu@gmail.com>
2016-06-28 16:24:06 -0400

    build

--- vendor.bundle.js
+++ vendor.bundle.js
@@ -100,7 +100,7 @@
 /***/ function(module, exports, __webpack_require__) {
 
 	__webpack_require__(2);
-	module.exports = __webpack_require__(74);
+	module.exports = __webpack_require__(75);
 
 
 /***/ },
@@ -9935,7 +9935,7 @@
 
 /***/ },
 
-/***/ 74:
+/***/ 75:
 /***/ function(module, exports, __webpack_require__) {
 
 	/* WEBPACK VAR INJECTION */(function(jQuery) {/*!
